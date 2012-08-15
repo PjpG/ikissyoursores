6d4aeea8fad34584e66fe954545ae3bbd341baa9
PjpG <patryk@doppio.jp>
2012-08-15 10:41:41 +0400

    First stage of page - basic layout, example images, first arch.

---
+++ app/controllers/info_controller.rb
@@ -0,0 +1,6 @@
+class InfoController < ApplicationController
+
+  def main
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IkissyoursoresPage::Application.config.secret_token = '63709b398e12308ecef6e5994d8bbf6cb83953edd12f486332939f262f1e04c8533b19ec13d37ac9160990034d460bf409f8c92fce71111774307d8aed3ed12e'
